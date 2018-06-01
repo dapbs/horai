71ce904a108b01b04c18059bbf3d8ddaaa10294b
Zeyad Deeb <za258362@stl1508008mac.ff.ffbs.net>
2018-06-01 17:17:53 -0500

    some changes

--- horai/SeasonalForecast.py
+++ horai/SeasonalForecast.py
@@ -1,44 +1,23 @@
 from .noaa import NOAA
-from .utils import AddMonths
+from .utils import add_months
 from shapely.geometry import shape, Point
-import urllib
-import io
-import shapefile
-from zipfile import ZipFile
 import re
 import datetime
 
 
 class SeasonalForecast:
     def __init__(self, weather_provider='noaa', forecast_type = 'temp', forecast_date = None):
-
         assert weather_provider in ['noaa','weather']
         self.weather_provider = weather_provider
         self.forecast_date = forecast_date
         self.forecast_type = forecast_type
 
-    def get_noaa_data(self):
-        latest_file_url = NOAA(forecast_type = self.forecast_type, forecast_date = self.forecast_date).get_latest_file_url()
-        url = latest_file_url[0]
-        start_date = latest_file_url[1]
-        end_date = latest_file_url[2]
-        response = urllib.request.urlopen(url).read()
-        noaa_files = ZipFile(io.BytesIO(response))
-        list_of_shps = sorted([fil for fil in noaa_files.namelist() if fil.endswith('.shp')])
-        list_of_shx = sorted([fil for fil in noaa_files.namelist() if fil.endswith('.shx')])
-        list_of_dbf = sorted([fil for fil in noaa_files.namelist() if fil.endswith('.dbf')])
-        shape_data = []
-        lead = []
-        for shpname, shxname, dbfname in zip(list_of_shps, list_of_shx, list_of_dbf):
-            #Retrieve correct lead order from shapefile name
-            lead.append(re.findall(r'\d+', shpname))
-            shape_data.append(shapefile.Reader(shp  = io.BytesIO(noaa_files.read(shpname)),
-                                                 shx  = io.BytesIO(noaa_files.read(shxname)),
-                                                 dbf  = io.BytesIO(noaa_files.read(dbfname))))
-        return shape_data,start_date,end_date,lead
+    def get_data(self):
+        if self.weather_provider == 'noaa':
+            d = NOAA(self.forecast_date).get_noaa_data()
+        return d
 
     def get_forecast(self, lon, lat):
-        noaa_data = self.get_noaa_data()
         weather_data = noaa_data[0]
         start_date = noaa_data[1]
         end_date = noaa_data[2]
